3ef486dfb394718516f32596752fe5da88d6c6d9
Ai Shi <shiai8901@gmail.com>
2017-04-06 21:58:24 -0700

    added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require('webpack');
+
+module.exports = {
+	entry: ['./client/index.js'],
+	output: {
+		path: __dirname + '/public/',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: '/node_modules/',
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			}
+		]
+	}
+}
